4e099b7375f1836646234f6463ddc4d1edeaf7e2
Vansh Chaurasiya <vanshchaurasiya1557@gmail.com>
2024-09-23 14:54:57 +0530

    👨‍💻Adding codes

---
+++ Unit3-Recursion/2_waterOverflow.cpp
@@ -0,0 +1,38 @@
+// GFG Link: https://www.geeksforgeeks.org/problems/champagne-overflow2636/1
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    double pourWater(double water, int r, int c)
+    {
+        if (r < 0 || c < 0 || c > r)
+            return 0;
+        if (r == 0 && c == 0)
+            return min(water, 1.0);
+        if (water > 1.0)
+        {
+            double extraWater = water - 1.0;
+            return min(1.0, water) + (pourWater(extraWater / 2.0, r - 1, c - 1) + pourWater(extraWater / 2.0, r - 1, c));
+        }
+        return min(1.0, water);
+    }
+
+    double waterOverflow(int K, int R, int C)
+    {
+        return pourWater((double)K, R, C);
+    }
+};
+
+int main()
+{
+    int k, r, c;
+    cin >> k >> r >> c;
+
+    Solution obj;
+    cout << fixed << setprecision(6) << obj.waterOverflow(k, r, c) << endl;
+
+    return 0;
+}
